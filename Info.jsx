72235dc8cf5e8f4d7999e039af1d973cacdaf4da
Dinuka <84348303+SilvaDH@users.noreply.github.com>
2021-06-28 11:58:36 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> Javascript & React</h1>
+      <p>A basic web dev React Js bootcamp</p>
+    </div>
+  );
+}
+export default Info;
